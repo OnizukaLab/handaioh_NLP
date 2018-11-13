72932d5362e015cf39617e9ae21e4302b9e9ee69
tanaka504 <tanan2-o-univ.kisasage@outlook.jp>
2018-11-13 22:02:22 +0900

    create digit candidate selector

--- mysite/handaioh_NLP/utils/Candidate_selector.py
+++ mysite/handaioh_NLP/utils/Candidate_selector.py
@@ -1,6 +1,7 @@
 from gensim.models import KeyedVectors
 from mysite.settings import BASE_DIR
 from pathlib import Path
+import random
 
 data_path = str(Path(BASE_DIR).joinpath('handaioh_NLP/utils/data/word2vec.300d.ja.txt').resolve())
 model = KeyedVectors.load_word2vec_format(data_path)
@@ -9,6 +10,15 @@ model = KeyedVectors.load_word2vec_format(data_path)
 def Candidate_selector(word, topn=3):
     return [word for (word, _) in model.most_similar(word, topn=topn)] if word in model.wv.vocab else None
 
-if __name__ == '__main__':
-    Candidate_selector('NASA')
+def digit_candidate(num):
+    Int_Frac = num.split('.')
+    Int_part = Int_Frac[0] if len(Int_Frac[0]) > 0 else '0'
+    Frac_part = str('.' + '.'.join(Int_Frac[1:])) if len(Int_Frac) > 1 else ''
+    margin = random.sample([i for i in range(-int(Int_part[0]) + 1, 10 - int(Int_part[0])) if not i == 0], 3)
+    return [str(m * (10 ** (len(Int_part) - 1)) + int(Int_part)) + Frac_part for m in margin]
 
+def quiz_generator():
+    return None
+
+if __name__ == '__main__':
+    quiz_generator()
