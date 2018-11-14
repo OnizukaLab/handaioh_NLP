605ade4876a9eb4f9219e54d1ffbbe8396fa9cab
tanaka504 <tanan2-o-univ.kisasage@outlook.jp>
2018-11-15 00:32:53 +0900

    fix dockerfile and make_spotlight

--- spotlight/make_spotlight.sh
+++ spotlight/make_spotlight.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker pull dbpedia/spotlight-japanese
-sudo docker build -f Dockerfile  -t dbpedia/spotlight-japanese .
-sudo docker run -i -p 2250:80 dbpedia/spotlight-japanese spotlight.sh
+docker pull dbpedia/spotlight-japanese
+docker build -f Dockerfile  -t dbpedia/spotlight-japanese .
+docker run -itd --name splotlight-jp -p 2250:80 dbpedia/spotlight-japanese spotlight.sh
